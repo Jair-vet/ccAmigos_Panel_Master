2f5855e0f1a587358a0d0ef429bc84e827ecbb53
Jair <jairaceves56@gmail.com>
2024-08-09 10:23:19 -0600

    Cambio de Ruta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
-export const environment = {
-  production: false,
-  base_url: 'http://localhost:4002/api',
-  clearFields: false,
-};
 // export const environment = {
-//   production: true,
-//   base_url: 'https://developer.binteapi.com:4003/api',
-//   clearFields: true,
+//   production: false,
+//   base_url: 'http://localhost:4002/api',
+//   clearFields: false,
 // };
+export const environment = {
+  production: true,
+  base_url: 'https://developer.binteapi.com:4003/api',
+  clearFields: true,
+};
